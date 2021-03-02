27bde48a38b24df93a34cd2ce347149c42461a8d
吉井駿一 <syun_syun01@icloud.com>
2021-03-02 11:44:50 +0900

    画像保存を実装

--- DeadManApp/ResultViewController.swift
+++ DeadManApp/ResultViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ResultViewController: UIViewController{
     
+    @IBOutlet weak var imageHoldView: UIView!
     @IBOutlet weak var resultImageView: UIImageView!
     var resultImage = UIImage(named: "defaultImage")
     @IBOutlet weak var saveButton: UIButton!
@@ -31,6 +32,61 @@ class ResultViewController: UIViewController{
         shareButton.layer.cornerRadius = 10
         shareButton.layer.borderColor = UIColor.gray.cgColor
         shareButton.layer.borderWidth = 1
-
+        
     }
+    
+    // UIViewをUIImageに変換
+    func viewToImage(_ view: UIView) -> UIImage{
+        
+        let rect = view.bounds
+        
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        let context: CGContext = UIGraphicsGetCurrentContext()!
+        
+        view.layer.render(in: context)
+        
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+    
+    // viewをimageに変換してカメラロールに保存
+    func saveImage(_ view: UIView){
+        
+        let image: UIImage = viewToImage(view)
+        
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(didFinishSavingImage(_:didFinishSavingWithError:contextInfo:)), nil)
+        
+    }
+    
+    // 画像の保存結果を取得
+    @objc func didFinishSavingImage(_ image: UIImage, didFinishSavingWithError error: NSError!, contextInfo: UnsafeMutableRawPointer){
+        
+        var title = "保存完了"
+        var message = "カメラロールに保存しました！"
+        
+        if error != nil{
+            title = "エラー"
+            message = "カメラロールに保存できませんでした"
+        }
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title:"OK", style:.default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+
+        
+    }
+    
+    @IBAction func tapSaveButton(_ sender: Any) {
+        
+        saveImage(imageHoldView)
+        
+    }
+    
+    @IBAction func tapShareButton(_ sender: Any) {
+    }
+    
 }
